3b7ffb53c07701472ef3957e67366223f92474a9
joanartdacosta <joanartdacosta@gmail.com>
2023-10-21 13:54:59 +0200

    typewriter npm installation

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,8 +1,4 @@
-import { Inter } from "next/font/google";
-// import homeStyles from "../components/home/home.module.css";
 import Title from "../components/home/Title";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
