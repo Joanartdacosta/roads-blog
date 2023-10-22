413d77dcf009be82cbde21ce1963c2742e2192c9
joanartdacosta <joanartdacosta@gmail.com>
2023-10-22 20:40:59 +0200

    correction: passeio replaced to trip

--- app/components/UI/cards/card_info/CardInfo.js
+++ app/components/UI/cards/card_info/CardInfo.js
@@ -7,33 +7,33 @@ export default function CardInfo(props) {
 
   return (
     <div>
-      {ARRAY.map((passeio) => {
+      {ARRAY.map((trip) => {
         return (
           <div>
             <a
-              href={passeio.href}
+              href={trip.href}
               class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
             >
               <img
                 class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                src={passeio.imgURL}
-                alt={passeio.title}
+                src={trip.imgURL}
+                alt={trip.title}
               />
               <div class="flex flex-col justify-between p-4 leading-normal">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {passeio.title}
+                  {trip.title}
                 </h5>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {passeio.paragraph1}
+                  {trip.paragraph1}
                 </p>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {passeio.paragraph2}
+                  {trip.paragraph2}
                 </p>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {passeio.paragraph3}
+                  {trip.paragraph3}
                 </p>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {passeio.paragraph4}
+                  {trip.paragraph4}
                 </p>
               </div>
             </a>
