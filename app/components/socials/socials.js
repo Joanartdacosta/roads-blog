1be8c9bfb4a0bb52723143fc899895006ba42bb4
joanartdacosta <joanartdacosta@gmail.com>
2023-10-22 14:57:01 +0200

    fix unique id from social gallery

--- app/components/socials/socials.js
+++ app/components/socials/socials.js
@@ -1,12 +1,12 @@
 const SOCIALS = [
   {
-    id: 1,
+    id: "social1",
     href: "https://www.instagram.com/",
     text: "instagram-icon",
     imgURL: "https://chalet-webapp.s3.amazonaws.com/icons/insta-icon.png",
   },
   {
-    id: 2,
+    id: "social2",
     href: "/contactos",
     text: "message-icon",
     imgURL: "https://chalet-webapp.s3.amazonaws.com/icons/message-icon.png",
