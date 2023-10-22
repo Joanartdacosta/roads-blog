9daeb9348be25083bf7726b15e8f3d21e95c1e10
joanartdacosta <joanartdacosta@gmail.com>
2023-10-22 20:38:42 +0200

    fix navbar's sections

--- app/components/navbar/NavBar.js
+++ app/components/navbar/NavBar.js
@@ -2,33 +2,37 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <div className="navbar">
+    <div className="navbar background">
       <div className="display-flex padding-40 display-column navbar-background">
         <Link className="link font-text padding-10" href="/sobre">
           Sobre
         </Link>
-        <Link className="link font-text padding-10" href="/miradouros">
-          Miradouros
+
+        <Link className="link font-text padding-10" href="/passadicos">
+          Passeios e Passadicos
+        </Link>
+        <Link className="link font-text padding-10" href="/roteiros">
+          Roteiros
         </Link>
 
-        <Link className="link font-text padding-10" href="/passeios">
-          Passeios
+        <Link
+          className="link font-text padding-10"
+          href="/roteiros/#roteirogratuito"
+        >
+          Roteiro Personalizado GRATUITO
         </Link>
         <Link className="link font-text padding-10" href="/geossitios">
           Geossitios
         </Link>
-        <Link className="link font-text padding-10" href="/passadicos">
-          Passadicos
+
+        <Link className="link font-text padding-10" href="/feiras">
+          Feiras, Romarias e Festivais
         </Link>
-        <Link className="link font-text padding-10" href="/humor">
-          Humor
-        </Link>
+
         <Link className="link font-text padding-10" href="/proverbios">
           Proverbio do Mes
         </Link>
-        <Link className="link font-text padding-10" href="/imprensa">
-          Artigos & Imprensa
-        </Link>
+
         <Link className="link font-text padding-10" href="/contactos">
           Contactos
         </Link>
