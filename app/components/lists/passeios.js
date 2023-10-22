d035fecf1879d5d636fbac574063c31e7560fd6c
joanartdacosta <joanartdacosta@gmail.com>
2023-10-22 19:47:13 +0200

    create arrays (lists) including info

---
+++ app/components/lists/passeios.js
@@ -0,0 +1,50 @@
+const PASSEIOS = [
+  {
+    id: "passadicos-1",
+    imgURL:
+      "https://barlavento.sapo.pt/wp-content/uploads/2021/08/Mina-de-Sal-gema-Loul%C3%A9-01.jpg",
+    href: "/passadicos/minas_loule",
+    title: "Passeio - Minas de Sal Gema de Loulé",
+    paragraph1:
+      "Passeio pelas minas de Sal Gema de Loule, um passeio bastante interessante",
+    district: "Faro",
+    town1: "Loule",
+    town2: "Sao Clemente",
+    access1:
+      "Sentido A2 »» A22 – Saída 12 Loulé Centro, direção Loulé e indicação “Minas de Sal Gema",
+    access2:
+      "Sentido Espanha »» A22 – Saída 13 Loulé Sul, direção Loulé e indicação “Minas de Sal Gema”",
+    advices:
+      " Aconselha-se o uso de calçado e pode-se usar qualquer material de recolha de imagem",
+    sources: "http://www.techsalt.pt/",
+  },
+  {
+    id: "passadicos-2",
+    title: "Passadicos do Mondega",
+    imgURL:
+      "https://images.impresa.pt/expresso/2023-03-28-passadicos-do-mondego3.jpg-a9238acb/3x2/mw-694",
+
+    paragraph1:
+      "Passeio pelas minas de Sal Gema de Loule, um passeio bastante interessante",
+    href: "/passadicos/mondego",
+
+    district: "Guarda",
+    town1: "Guarda",
+    town2: "Videmonte, Corujeira e Trinta e Mizarela/Pero Soares e Vila Soeiro",
+    distance: "12km",
+
+    access1: "A25 - A23 – Guarda – Prado – Barragem do Caldeirão",
+    access2:
+      "   A23 – Sair em Belmonte N18 direção Guarda – Prado - Barragem do Caldeirão",
+    advices1:
+      " Estação do ano sugerida:  Primavera/Outono com início pela manhã.",
+    advices2: "Até abril de 2023 não havia nem um local de descanso.",
+    advices3: "A meio do percurso há uma pequena barraca que vende café e mel.",
+    direction1:
+      "Sentido Videmonte – Barragem é mais suave para início, mas depois a parte final obriga à subida de cerca de 700 degraus, dificil",
+    directions2: "Sentido Barragem – Videmonte – mais aconselhada",
+    sources: "https://www.passadicosdomondego.pt/",
+  },
+];
+
+export default PASSEIOS;
