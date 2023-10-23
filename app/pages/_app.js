9a81e8d94778abaaf35f0aa7b390cdde807c1074
joanartdacosta <joanartdacosta@gmail.com>
2023-10-23 10:24:31 +0200

    MUI installation

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout/Layout";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {...pageProps} />;
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Component {...pageProps} />;
+      </LocalizationProvider>
     </Layout>
   );
 }
