7b0a42e4e1d2179a5a3d4b3cd79ba45593ef9969
joanartdacosta <joanartdacosta@gmail.com>
2023-10-26 22:42:29 +0200

    Banner - separate components

---
+++ app/components/UI/banner/Banner copy.js
@@ -0,0 +1,11 @@
+import BannerGallery from "@/components/UI/banner/BannerGallery";
+import BannerDescription from "@/components/UI/banner/FreeTourDescription";
+
+export default function Banner() {
+  return (
+    <div>
+      <BannerDescription />
+      <BannerGallery />
+    </div>
+  );
+}
