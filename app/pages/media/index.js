f3b7131d58348c3796e5fe1cee32390f8dffe8a7
joanartdacosta <joanartdacosta@gmail.com>
2023-11-03 20:27:22 +0100

    include a banner in all index pages

--- app/pages/media/index.js
+++ app/pages/media/index.js
@@ -1,10 +1,13 @@
+import Banner from "@/components/common/banner/Banner";
 import React from "react";
 
 export default function Media() {
   return (
     <div>
-      <h1 className="text-center font-title">MEDIA</h1>
-      <p>Videos, artigos e noticias</p>
+      <Banner />
+      <div className="max-w-5xl m-auto pt-20 pb-20">
+        <p>Videos, artigos e noticias</p>
+      </div>
     </div>
   );
 }
