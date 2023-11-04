a6c2015ebe4bc0a876b0562ecc16b42cf4fe0182
joanartdacosta <joanartdacosta@gmail.com>
2023-11-04 12:16:43 +0100

    fix forms

--- app/components/common/UI/forms/details/Select.js
+++ app/components/common/UI/forms/details/Select.js
@@ -17,7 +17,7 @@ export default function Select(props) {
         </select>
 
         <label
-          for="floating_company"
+          for="transport"
           class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-yellow-600 peer-focus:dark:text-yellow-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
           {props.label}
