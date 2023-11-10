2a95d31bb1de3c6542fcf19988f00a0c972b3993
joanartdacosta <joanartdacosta@gmail.com>
2023-11-10 14:01:06 +0100

    styling fair cards

--- app/components/dashboard/fairs/UI/fairs_card/FairLocation.js
+++ app/components/dashboard/fairs/UI/fairs_card/FairLocation.js
@@ -1,10 +1,11 @@
 export default function FairLocation(props) {
   return (
     <div>
-      <div className="text-base font-normal">
-        <span className="font-medium text-gray-900 dark:text-white"></span>{" "}
-        {props.title}
-        <span className="font-medium text-gray-900 dark:text-white">
+      <div>
+        <span className="text-base font-bold text-gray-900">{props.title}</span>
+      </div>
+      <div>
+        <span className="text-sm text-gray-800">
           {props.district} || {props.town2}
         </span>{" "}
       </div>
