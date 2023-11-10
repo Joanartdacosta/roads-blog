3126f98b76ad0ac5082cf62d5df668f25c107756
joanartdacosta <joanartdacosta@gmail.com>
2023-11-10 20:09:38 +0100

    cleaning code - bignavbar

--- app/components/common/navbar/big_navbar/NavBar.js
+++ app/components/common/navbar/big_navbar/NavBar.js
@@ -8,16 +8,16 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import SmallFooter from "../SmallFooter";
-import NAVBAR_LINKS from "@/components/lists/navbar_links";
+import NAVBAR_LINKS from "@/components/lists/navbar_links_map";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function NavBarBig() {
   const [state, setState] = React.useState({
-    top: false,
-    bottom: false,
     menu: true,
   });
+
+  const anchor = "menu";
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (
@@ -32,19 +32,22 @@ export default function NavBarBig() {
 
   const list = (anchor) => (
     <Box
-      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      sx={{ width: 250 }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
       className="overflow-hidden pl-0 w-full p-5 h-full bg-black"
     >
       <List className=" text-white overflow-hidden">
-        {NAVBAR_LINKS.map((item) => (
-          <ListItem key={item.section} disablePadding>
+        {NAVBAR_LINKS["order"].map((item) => (
+          <ListItem key={NAVBAR_LINKS[item].section} disablePadding>
             <ListItemButton>
               <ListItemIcon></ListItemIcon>
-              <Link href={item.href} className="hover:text-yellow-400">
-                <ListItemText primary={item.section} />
+              <Link
+                href={NAVBAR_LINKS[item].href}
+                className="hover:text-yellow-400"
+              >
+                <ListItemText primary={NAVBAR_LINKS[item].section} />
               </Link>
             </ListItemButton>
           </ListItem>
@@ -66,23 +69,22 @@ export default function NavBarBig() {
         height={"100"}
         className="m-2 float-right"
       />
-      {["menu", "top", "bottom"].map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button
-            className="overflow-hidden text-gray-300 text-lg h-28 pl-20"
-            onClick={toggleDrawer(anchor, true)}
-          >
-            {anchor}
-          </Button>
-          <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-          >
-            {list(anchor)}
-          </Drawer>
-        </React.Fragment>
-      ))}
+
+      <React.Fragment key={anchor}>
+        <Button
+          className="overflow-hidden text-gray-300 text-lg h-28 pl-20"
+          onClick={toggleDrawer(anchor, true)}
+        >
+          {anchor}
+        </Button>
+        <Drawer
+          anchor={anchor}
+          open={state[anchor]}
+          onClose={toggleDrawer(anchor, false)}
+        >
+          {list(anchor)}
+        </Drawer>
+      </React.Fragment>
     </div>
   );
 }
