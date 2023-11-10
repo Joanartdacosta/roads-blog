50bd1dd55de515dfe3e4a30d0546f48ef472291f
joanartdacosta <joanartdacosta@gmail.com>
2023-11-10 13:45:53 +0100

    breaking MenuDescription into smaller parts

---
+++ app/components/dashboard/tour/menus/menu_description/elements/MenuDescriptionValues.js
@@ -0,0 +1,17 @@
+export default function MenuDescriptionValues(props) {
+  return (
+    <div
+      class="bg-white rounded-lg dark:bg-gray-800"
+      id={props.id}
+      role="tabpanel"
+      aria-labelledby={props.id}
+    >
+      <dl class="m-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-6 dark:text-white sm:p-8">
+        <div class="flex flex-col items-center justify-center">
+          <dt class="mb-2 text-3xl font-extrabold">{props.value}</dt>
+          <dd class="text-gray-500 dark:text-gray-400">{props.subtitle}</dd>
+        </div>
+      </dl>
+    </div>
+  );
+}
--- app/pages/tours/index.js
+++ app/pages/tours/index.js
@@ -1,9 +1,8 @@
 import React from "react";
-
-import Form from "@/components/common/UI/forms/Form";
 import CardStars from "@/components/common/UI/cards/card_stars/CardStars";
 import TOURS from "@/components/lists/tours";
 import GeneralBanner from "@/components/common/general_banner/GeneralBanner";
+import MenuDescription from "@/components/dashboard/tour/menus/menu_description/MenuDescription";
 
 export default function Tours() {
   return (
@@ -11,6 +10,7 @@ export default function Tours() {
       <GeneralBanner />
       <div className="max-w-5xl m-auto pt-20 pb-20 w-max">
         <CardStars array={TOURS} />
+        <MenuDescription />
       </div>
     </div>
   );
