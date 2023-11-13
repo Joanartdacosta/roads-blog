f25d770d972d90b53dbd1773909964c6dfb71894
joanartdacosta <joanartdacosta@gmail.com>
2023-11-13 14:52:30 +0100

    add new component - introduction

---
+++ app/components/dashboard/about/Introduction.js
@@ -0,0 +1,29 @@
+export default function Introduction() {
+  return (
+    <div className=" m-auto pb-20 text-center pt-20">
+      <div className="pb-10 max-w-2xl">
+        <p>
+          Chamo-me Rui Costa e fui professor de Geografia ao longo de 42 anos.
+        </p>
+        <p>
+          A experiência como professor fez crescer o meu gosto por viajar e
+          levou-me a organizar muitos passeios, como professor e tambem para a
+          família.{" "}
+        </p>
+        <p>
+          {" "}
+          Agora que me encontro, numa nova etapa da minha vida (aposentado),
+          achei que podia ajudar, quem gosta de viajar, testemunhando as minhas
+          experiências, partilhando roteiros das viagens e divulgando aspetos de
+          interesse geográfico.
+        </p>
+      </div>
+
+      <h2 className="italic">
+        A vida é o que fazemos dela. As viagens são os viajantes. O que vemos
+        não é o que vemos, senão o que somos.
+      </h2>
+      <p>Fernando Pessoa</p>
+    </div>
+  );
+}
