3b292aef76af6d5e6fa1043c0b047c900bca29a4
joanartdacosta <joanartdacosta@gmail.com>
2023-11-13 14:11:46 +0100

    add new text color - amber

--- app/components/common/footer/Footer.js
+++ app/components/common/footer/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div className="text-center bg-black text-yellow-300 bottom-0 fixed w-full left-0 ">
-      <p className="italic p-5 text-base">
+    <div className="text-center bg-black text-amber-500 bottom-0 fixed w-full left-0 ">
+      <p className="italic p-5 text-xs">
         Copyright. Direitos reservados - Rui Costa. Website developed by Joana
         Costa.
       </p>
