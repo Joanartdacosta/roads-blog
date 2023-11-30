998bb7d9c939e94f2f8d24d61a6b7a3c68e9a5fc
joanartdacosta <joanartdacosta@gmail.com>
2023-11-30 13:02:27 +0100

    colors to bars - red and yellow

---
+++ app/components/dashboard/tour/classification/other_colors.js
@@ -0,0 +1,11 @@
+{
+  /* class="bg-green-600 h-2.5 rounded-full dark:bg-green-500" */
+}
+
+{
+  /* class="bg-yellow-400 h-2.5 rounded-full" */
+}
+
+{
+  /* class="bg-red-600 h-2.5 rounded-full dark:bg-red-500" */
+}
