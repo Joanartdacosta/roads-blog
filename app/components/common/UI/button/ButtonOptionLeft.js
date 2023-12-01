c7cb4aeed8321e1e2aae7d13f395b12b4544d69e
joanartdacosta <joanartdacosta@gmail.com>
2023-12-01 13:31:42 +0100

    change fonts, title style

--- app/components/common/UI/button/ButtonOptionLeft.js
+++ app/components/common/UI/button/ButtonOptionLeft.js
@@ -5,7 +5,7 @@ export default function ButtonOptionLeft(props) {
         onClick={props.onClick}
         type="button"
         value={props.value}
-        className="rounded-l-md border px-4 py-2 text-sm font-medium text-gray-900 bg-white border-gray-200 hover:bg-gray-100 hover:text-yellow-700 focus:z-10 focus:ring-2 focus:ring-yellow-700 focus:text-yellow-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-yellow-500 dark:focus:text-white"
+        className="play rounded-l-md border px-4 py-2 text-sm font-medium text-gray-900 bg-white border-gray-200 hover:bg-gray-100 hover:text-yellow-700 focus:z-10 focus:ring-2 focus:ring-yellow-700 focus:text-yellow-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-yellow-500 dark:focus:text-white"
       >
         {props.label}
       </button>
