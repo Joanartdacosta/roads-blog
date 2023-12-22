d01cb22cbb1679f9813ee8325ed0e185407fd6c4
joanartdacosta <joanartdacosta@gmail.com>
2023-12-22 15:27:18 +0100

    optimize video with cloudinary

--- app/components/dashboard/home/video/HomeVideo.js
+++ app/components/dashboard/home/video/HomeVideo.js
@@ -1,3 +1,6 @@
+import { CldVideoPlayer } from "next-cloudinary";
+// import { Video } from "cloudinary-react";
+
 export default function HomeVideo() {
   const videoCar = "https://estradas-website.s3.amazonaws.com/Estradas/car.mp4";
 
@@ -11,16 +14,29 @@ export default function HomeVideo() {
   //   "https://estradas-website.s3.amazonaws.com/Estradas/road-landscape.mp4";
 
   return (
-    <div className="video">
-      {" "}
+    // <div className="video">
+    //   {" "}
+    //   <video
+    //     autoplay="autoplay"
+    //     muted
+    //     loop="loop"
+    //     className="video object-cover w-screen h-screen fixed"
+    //   >
+    //     <source src={videoTrees} type="video/mp4" />
+    //   </video>
+    // </div>
+
+    <div>
+      <CldVideoPlayer width="1920" height="1080" src="floresta_gkomom" />
+      {/* 
       <video
-        autoplay="autoplay"
-        muted
-        loop="loop"
-        className="video object-cover w-screen h-screen fixed"
-      >
-        <source src={videoTrees} type="video/mp4" />
-      </video>
+        autoPlay
+        cloudName="dij4z66rp"
+        publicId="floresta_gkomom"
+        width="640"
+        height="360"
+        controls
+      /> */}
     </div>
   );
 }
