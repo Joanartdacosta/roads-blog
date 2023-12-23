51e7823b176e2f9739c29b1d25caad87ea120649
joanartdacosta <joanartdacosta@gmail.com>
2023-12-23 11:07:23 +0100

    correction: replace 'class' to 'className'

--- app/components/common/menu_id/path/Path.js
+++ app/components/common/menu_id/path/Path.js
@@ -15,7 +15,7 @@ export default function Path(props) {
         <button
           aria-expanded="false"
           aria-controls="accordion-flush-body-2"
-          class="flex items-center justify-between w-full py-5 font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
+          className="flex items-center justify-between w-full py-5 font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
           data-accordion-target="#accordion-flush-body-2"
           onClick={setVisible}
           type="button"
