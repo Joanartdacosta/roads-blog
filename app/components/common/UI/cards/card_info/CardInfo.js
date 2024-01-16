a7a06aa27e91e02a1fa4318df50540fd0c697553
joanartdacosta <joanartdacosta@gmail.com>
2024-01-16 18:36:44 +0100

    add source and price on passport detail page

--- app/components/common/UI/cards/card_info/CardInfo.js
+++ app/components/common/UI/cards/card_info/CardInfo.js
@@ -16,14 +16,7 @@ export default function CardInfo(props) {
               href={trip.href}
               className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
             >
-              <Image
-                alt={"foto de" + trip.title}
-                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                layout="fill"
-                src={trip.imgUrl}
-                unoptimized
-              />
-              <div className="flex flex-col justify-between p-4 leading-normal">
+              <div className="p-4">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                   {trip.title}
                 </h5>
@@ -39,6 +32,16 @@ export default function CardInfo(props) {
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                   {trip.p4}
                 </p>
+
+                <h2 className="text-lg font-extrabold tracking-tight text-gray-900 dark:text-white">
+                  Preco:
+                </h2>
+                <p className="mb-5"> {trip.price}</p>
+
+                <h2 className="text-lg font-extrabold tracking-tight text-gray-900 dark:text-white">
+                  Fonte:
+                </h2>
+                <p className="mb-5"> {trip.source}</p>
               </div>
             </a>
           </div>
