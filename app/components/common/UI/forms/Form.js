93684a04429305ecce507b257947d0a07124b372
joanartdacosta <joanartdacosta@gmail.com>
2024-02-05 15:35:03 +0100

    fix sending email

--- app/components/common/UI/forms/Form.js
+++ app/components/common/UI/forms/Form.js
@@ -1,72 +1,49 @@
 import BUTTONS_LABELS from "@/components/enums/buttons_labels";
+import FORM_LABELS from "@/components/enums/form_labels";
 import Input from "./details/Input";
-import Select from "./details/Select";
 import SubmitButton from "./details/SubmitButton";
 import TextArea from "./details/TextArea";
-import FORM_LABELS from "@/components/enums/form_labels";
+
+import { useState } from "react";
 
 export default function Form() {
   const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
   const [name, setName] = useState("");
-  const [departure, setDeparture] = useState("");
+  const [message, setMessage] = useState("");
   const [arrival, setArrival] = useState("");
-
-  const [submitted, setSubmitted] = useState(false);
-  const [error, setError] = useState("");
-
-  function submit(e) {
-    e.preventDefault();
-
-    fetch("https://formcarry.com/s/XXXXXXX", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        email: email,
-        name: name,
-        arrival: arrival,
-        departure: departure,
-        message: message,
-      }),
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        if (res.code === 200) {
-          setSubmitted(true);
-        } else {
-          setError(res.message);
-        }
-      })
-      .catch((error) => setError(error));
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  if (submitted) {
-    return <p>We've received your message, thank you for contacting us!</p>;
-  }
+  const [departure, setDeparture] = useState("");
 
   return (
-    <form className="pb-20" id="freetour" onSubmit={submit}>
+    <form
+      className="pb-20"
+      id="freetour"
+      action="https://formcarry.com/s/yq6rkztaug1"
+      method="POST"
+    >
       <Input
         type={"text"}
         label={FORM_LABELS.name}
         value={name}
+        name="name"
         onChange={(e) => setName(e.target.value)}
+        required
       />
       <Input
         type={"email"}
         label={FORM_LABELS.email}
         value={email}
+        name="email"
         onChange={(e) => setEmail(e.target.value)}
         required
       />
-      <TextArea />
+      <TextArea
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        id="message"
+        name="message"
+        placeholder="Enter your message..."
+        required
+      />
 
       <div className="pt-10 pb-10">
         <p>Para solicitar o roteiro gratuito, estes campos sao obrigatorios:</p>
@@ -78,26 +55,18 @@ export default function Form() {
           label={FORM_LABELS.departurePoint}
           value={departure}
           onChange={(e) => setDeparture(e.target.value)}
+          name="departure"
         />
         <Input
           type={"text"}
           label={FORM_LABELS.arrivalPoint}
           value={arrival}
+          name="arrival"
           onChange={(e) => setArrival(e.target.value)}
         />
       </div>
 
-      <div className="grid md:grid-cols-2 md:gap-6">
-        <Select label={FORM_LABELS.message} />
-        <Input
-          type={"text"}
-          label={FORM_LABELS.others}
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-      </div>
-
-      <SubmitButton label={BUTTONS_LABELS.sendMessage} />
+      <SubmitButton id="submit" label={BUTTONS_LABELS.sendMessage} />
     </form>
   );
 }
