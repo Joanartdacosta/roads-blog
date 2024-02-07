a11c1b7d776fb3530a776f5933dd77a4b8646a02
joanartdacosta <joanartdacosta@gmail.com>
2024-02-07 16:06:17 +0100

    fix homepage's button

--- app/components/common/UI/button/ButtonBlack.js
+++ app/components/common/UI/button/ButtonBlack.js
@@ -1,21 +1,23 @@
+import Link from "next/link";
+
 export default function ButtonBlack(props) {
   if (props.href) {
     return (
-      <button
+      <Link
         href={props.href}
-        className="play text-white border opacity-80 border-gray-900 border bg-black hover:bg-gray-100 hover:text-black focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium text-xl px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+        className="poppins text-sm text-gray-400 border opacity-90 border-gray-900 border bg-black hover:bg-gray-100 hover:text-black focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium text-xl px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
       >
         {props.label}
-      </button>
+      </Link>
     );
   }
 
   return (
-    <button
+    <Link
       onClick={props.onClick}
-      className="play text-white rounded-lg bg-black hover:bg-gray-100 hover:text-black focus:outline-none font-medium text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 "
+      className=" rounded-lg bg-black hover:bg-gray-100 hover:text-black focus:outline-none font-medium text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 "
     >
       {props.label}
-    </button>
+    </Link>
   );
 }
