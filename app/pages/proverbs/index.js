e1850b4de2e6c18e266abf6a6d3487d30e6a107d
joanartdacosta <joanartdacosta@gmail.com>
2024-02-22 17:08:46 +0100

    load info from mongodb and add it to index proverbs

--- app/pages/proverbs/index.js
+++ app/pages/proverbs/index.js
@@ -1,11 +1,11 @@
 import CardProverb from "@/components/common/UI/cards/card_info/CardProverb";
-import { getFeaturedProverbs } from "@/helpers/api-util";
+import { getProverbs } from "@/helpers/api-util";
 import { RevealWrapper } from "next-reveal";
 
 export default function Proverbs(props) {
   return (
-    <div className="roboto m-auto justify-items-center">
-      <div className="max-w-5xl m-auto pt-20 pb-20 w-max">
+    <div className="justify-items-center">
+      <div className="m-auto w-max">
         <RevealWrapper>
           <CardProverb array={props.proverbs} />
         </RevealWrapper>
@@ -15,7 +15,7 @@ export default function Proverbs(props) {
 }
 
 export async function getStaticProps() {
-  const featuredProverbs = await getFeaturedProverbs();
+  const featuredProverbs = await getProverbs();
 
   return {
     props: {
