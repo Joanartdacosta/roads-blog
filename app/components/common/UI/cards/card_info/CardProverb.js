e9de9cc7646e86bd4c22285ad813ea2872819f26
joanartdacosta <joanartdacosta@gmail.com>
2024-02-22 17:09:12 +0100

    adapt card proverb according info of mongodb

--- app/components/common/UI/cards/card_info/CardProverb.js
+++ app/components/common/UI/cards/card_info/CardProverb.js
@@ -1,42 +1,28 @@
 export default function CardProverb(props) {
-  const ARRAY = [];
-
-  for (let i = 0; i < props.array.length; i++) {
-    ARRAY.push(props.array[i]);
-  }
-
   return (
-    <div className="m-auto">
-      {ARRAY.map((element) => {
+    <div className="m-auto mb-20 mt-20">
+      {props.array.map((element) => {
         return (
-          <div>
-            <a
-              href={element.href}
-              className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-            >
-              <img
-                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                src={element.imgUrl}
-                alt={element.title}
-              />
-              <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {element.title}
-                </h5>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {element.p1}
-                </p>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {element.p2}
-                </p>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {element.p3}
-                </p>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {element.p4}
-                </p>
+          <div
+            key={element.id}
+            className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-2xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+          >
+            <img
+              className="object-cover h-96 w-96 rounded-t-lg md:h-auto  md:rounded-none md:rounded-l-lg"
+              src={element.imgUrl}
+              alt={element.title}
+            />
+            <div className="flex flex-col justify-between leading-normal text-center">
+              <h5 className="m-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {element.title}
+              </h5>
+
+              <div className="mb-10 font-normal text-lg text-center poppins text-gray-700 dark:text-gray-400">
+                {props.array[0].p.map((p) => {
+                  return <p>{p}</p>;
+                })}
               </div>
-            </a>
+            </div>
           </div>
         );
       })}
