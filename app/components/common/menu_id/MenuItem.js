8d1ea61be320edd9127bf0b214010545e17bb370
joanartdacosta <joanartdacosta@gmail.com>
2024-02-22 21:49:54 +0100

    add responsive menu

--- app/components/common/menu_id/MenuItem.js
+++ app/components/common/menu_id/MenuItem.js
@@ -1,21 +1,39 @@
+import Link from "next/link";
+
 export default function MenuItem(props) {
   return (
     <div>
-      <li className="w-full">
-        <button
-          id={props.id}
-          data-tabs-target={props.href}
-          type="button"
-          role="tab"
-          aria-controls={props.id}
-          aria-selected="true"
-          value={props.label}
-          className="inline-block w-full p-4 rounded-tl-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600"
-          onClick={props.onClick}
-        >
-          {props.label}
-        </button>
-      </li>
+      <div className="w-full menu-labels">
+        <Link onClick={props.onClick} href={props.href}>
+          <button
+            id={props.id}
+            type="button"
+            role="tab"
+            aria-controls={props.id}
+            aria-selected="true"
+            value={props.label}
+            className="inline-block w-full p-4 rounded-tl-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600"
+            onClick={props.onClick}
+          >
+            {props.label}
+          </button>
+        </Link>
+      </div>
+      <div className="menu-icons w-24 m-auto">
+        <Link onClick={props.onClick} href={props.href}>
+          <button
+            id={props.id}
+            type="button"
+            role="tab"
+            aria-controls={props.id}
+            aria-selected="true"
+            value={props.label}
+            className="inline-block w-full p-4 rounded-tl-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600"
+          >
+            {props.icon}
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
