45881b1a5334d4d6901e4b6b19af39bebf9e5af2
joanartdacosta <joanartdacosta@gmail.com>
2024-02-26 20:51:17 +0100

    add buttons

---
+++ app/components/common/review/buttons/ButtonPost.js
@@ -0,0 +1,12 @@
+export default function ButtonPost() {
+  return (
+    <div>
+      <button
+        type="submit"
+        class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+      >
+        Post comment
+      </button>
+    </div>
+  );
+}
