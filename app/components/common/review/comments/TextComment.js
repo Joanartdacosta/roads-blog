ed706fa12cb2658a8c540bcf3c32f81f7638f7be
joanartdacosta <joanartdacosta@gmail.com>
2024-02-26 20:50:17 +0100

    comment description

---
+++ app/components/common/review/comments/TextComment.js
@@ -0,0 +1,10 @@
+export default function TextComment() {
+  return (
+    <div>
+      <p class="text-gray-500 dark:text-gray-400">
+        Thanks for sharing this. I do came from the Backend development and
+        explored some of the tools to design my Side Projects.
+      </p>
+    </div>
+  );
+}
